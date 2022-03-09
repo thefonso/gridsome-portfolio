9ba03d68d071dba8a8a660476b8610745a110f05
alfonso <unclefonso@protonmail.com>
2022-03-08 22:22:16 -0500

    adding side menu thing

---
+++ src/layouts/Default.vue
@@ -0,0 +1,70 @@
+<template>
+    <v-app>
+      <v-navigation-drawer permanent app color="blue-grey lighten-5" width="200">
+        <v-list-item>
+          <v-list-item-content>
+            <v-list-item-title class="title">
+              Application
+            </v-list-item-title>
+            <v-list-item-subtitle>
+              subtext
+            </v-list-item-subtitle>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-divider></v-divider>
+
+        <v-list
+            dense
+            nav
+        >
+          <v-list-item
+              v-for="item in items"
+              :key="item.title"
+              link
+          >
+            <v-list-item-icon>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-icon>
+
+            <v-list-item-content>
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+
+      <v-main>
+        <slot/>
+      </v-main>
+    </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      items: [
+        { title: 'Dashboard', icon: 'mdi-view-dashboard' },
+        { title: 'Photos', icon: 'mdi-image' },
+        { title: 'About', icon: 'mdi-help-box' },
+      ],
+      right: null,
+    }
+  },
+}
+</script>
+
+<static-query>
+query {
+  metadata {
+    siteName
+  }
+}
+</static-query>
+
+<style>
+/*.v-application--wrap{*/
+/*  flex-direction: row;*/
+/*}*/
+</style>
